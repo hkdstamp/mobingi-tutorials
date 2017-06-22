0d97ab776d79023d95456ee335dab4e25c94b816
Haruhiko Kodo <kodo.haruhiko@mobingi.com>
2017-06-22 13:51:35 +0900

    Update config

--- mobingi-install.sh
+++ mobingi-install.sh
@@ -1,4 +1,5 @@
-chmod -R 777 /var/www/html/php-fuel/developer/Sites/blog/fuel/app/logs
-mkdir /fuel/
-mkdir /fuel/logs/
-chmod -R 777 /fuel/logs/
+chmod -R 777 /var/www/html/php-fuel/developer/Sites/blog/fuel/app/tmp
+chmod -R 777 /var/www/html/php-fuel/developer/Sites/blog/fuel/app/cache
+mkdir /var/log/fuel/
+mkdir /var/log/fuel/logs/
+chmod -R 777 /var/log/fuel/logs/
